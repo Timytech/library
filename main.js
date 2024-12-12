e466d6361058d3568e2933772878995d6775430c
Timothy Gbadegesin <124510604+Timytech@users.noreply.github.com>
2024-12-12 12:43:53 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-Book.prototype.toggleRead = function() {
-    this.read = !this.read;
+    toggleRead() {
+        this.read = !this.read;
+    }
 }
 
 function addBookToLibrary(title, author, pages, read) {
@@ -20,6 +22,7 @@ function addBookToLibrary(title, author, pages, read) {
 function displayBooks() {
     const libraryDiv = document.getElementById('library');
     libraryDiv.innerHTML = ''; // Clear previous entries
+
     myLibrary.forEach((book, index) => {
         const bookCard = document.createElement('div');
         bookCard.classList.add('book-card');
